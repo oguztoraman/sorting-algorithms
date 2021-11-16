c8f2e7c3093bde9fb78f7d96dffe2b20d204ebb6
Oğuz Toraman <oguz.toraman@protonmail.com>
2021-11-16 22:02:09 +0300

    increase readability

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using T = int;
 
 using namespace project;
 
+sorting_algorithms<T> algorithms;
+
 const auto default_test_count{
     std::to_string(sorting_algorithms<T>::default_test_count)
 };
@@ -47,102 +49,107 @@ const auto help_message{
     "author: Oğuz Toraman <oguz.toraman@protonmail.com> @2021"
 };
 
-bool is_number(const std::string_view& sv)
-{
-    return (sv.find_first_not_of("0123456789") == std::string::npos);
-}
+bool is_number(const std::string_view& sv);
+void check_argumants(const std::string& input_size_or_file);
+void check_argumants(const std::string& input_size_or_file,
+                     const std::string& file_number_or_test_number, bool& compare);
+void check_argumants(const std::string& input_size_or_file,
+                     const std::string& file_number_or_test_number,
+                     const std::string& input_size, bool& compare);
 
 int main(int argc, char *argv[])
 {
     try {
         bool compare{true};
-        bool throw_help_message{false};
-        sorting_algorithms<T> algorithms;
         switch (argc){
         case 1 : break;
-        case 2 : {
-            const std::string input_size_or_file{argv[1]};
-            if ("-h"         == input_size_or_file ||
-                "--help"     == input_size_or_file ||
-                "-gen"       == input_size_or_file ||
-                "--generate" == input_size_or_file){
-                throw_help_message = true;
-            } else if (!is_number(input_size_or_file)){
-                algorithms.set(input_size_or_file);
-            } else {
-                algorithms.set(std::stoll(input_size_or_file));
-            }
-            break;
-        }
-        case 3 : {
-            if ("-gen"       == std::string{argv[1]} ||
-                "--generate" == std::string{argv[1]}){
-                const std::string file_number{argv[2]};
-                if (is_number(file_number)){
-                    compare = false;
-                    sorting_algorithms<T>::generate_input_files(
-                        sorting_algorithms<T>::default_input_size,
-                        std::stoll(file_number)
-                    );
-                    std::cout << "generated " << file_number
-                              << " files, each containing "
-                              << default_input_size
-                              << " random inputs\n";
-                } else {
-                    throw_help_message = true;
-                }
-            } else if (const std::string test_number{argv[2]};
-                       is_number(test_number)){
-                const std::string input_size_or_file{argv[1]};
-                if (!is_number(input_size_or_file)){
-                    algorithms.set(
-                        input_size_or_file,
-                        std::stoll(test_number)
-                    );
-                } else {
-                    algorithms.set(
-                        std::stoul(input_size_or_file),
-                        std::stoll(test_number)
-                    );
-                }
-            } else {
-                throw_help_message = true;
-            }
-            break;
-        }
-        case 4 : {
-            if ("-gen"       == std::string{argv[1]} ||
-                "--generate" == std::string{argv[1]}){
-                const std::string file_number{argv[2]}, input_size{argv[3]};
-                if (is_number(file_number) && is_number(input_size)){
-                    compare = false;
-                    sorting_algorithms<T>::generate_input_files(
-                        std::stoll(input_size),
-                        std::stoll(file_number)
-                    );
-                    std::cout << "generated " << file_number
-                              << " files, each containing "
-                              << input_size << " random inputs\n";
-                } else {
-                    throw_help_message = true;
-                }
-            } else {
-                throw_help_message = true;
-            }
-            break;
-        }
-        default: throw_help_message = true;
-        }
-        if (throw_help_message){
-            throw std::runtime_error{help_message};
+        case 2 : check_argumants(argv[1]); break;
+        case 3 : check_argumants(argv[1], argv[2], compare); break;
+        case 4 : check_argumants(argv[1], argv[2], argv[3], compare); break;
+        default: throw std::runtime_error{help_message};
         }
         if (compare){
-            std::cout << algorithms.compare(
-                sorting_algorithms<T>::all
-            );
+            std::cout << algorithms.compare(sorting_algorithms<T>::all);
         }
     } catch (const std::exception& e) {
         std::cerr << e.what() << "\n";
     }
     return 0;
+}
+
+bool is_number(const std::string_view& sv)
+{
+    return (sv.find_first_not_of("0123456789") == std::string::npos);
+}
+
+void check_argumants(const std::string& input_size_or_file)
+{
+    if ("-h"         == input_size_or_file ||
+        "--help"     == input_size_or_file ||
+        "-gen"       == input_size_or_file ||
+        "--generate" == input_size_or_file){
+        throw std::runtime_error{help_message};
+    } else if (!is_number(input_size_or_file)){
+        algorithms.set(input_size_or_file);
+    } else {
+        algorithms.set(std::stoll(input_size_or_file));
+    }
+}
+
+void check_argumants(const std::string& input_size_or_file,
+                     const std::string& file_number_or_test_number, bool& compare)
+{
+    if ("-gen"       == input_size_or_file ||
+        "--generate" == input_size_or_file){
+        if (is_number(file_number_or_test_number)){
+            compare = false;
+            sorting_algorithms<T>::generate_input_files(
+                sorting_algorithms<T>::default_input_size,
+                std::stoll(file_number_or_test_number)
+            );
+            std::cout << "generated " << file_number_or_test_number
+                      << " files, each containing "
+                      << default_input_size
+                      << " random inputs\n";
+        } else {
+            throw std::runtime_error{help_message};
+        }
+    } else if (is_number(file_number_or_test_number)){
+        if (!is_number(input_size_or_file)){
+            algorithms.set(
+                input_size_or_file,
+                std::stoll(file_number_or_test_number)
+            );
+        } else {
+            algorithms.set(
+                std::stoll(input_size_or_file),
+                std::stoll(file_number_or_test_number)
+            );
+        }
+    } else {
+        throw std::runtime_error{help_message};
+    }
+}
+
+void check_argumants(const std::string& input_size_or_file,
+                     const std::string& file_number_or_test_number,
+                     const std::string& input_size, bool& compare)
+{
+    if ("-gen"       == input_size_or_file ||
+        "--generate" == input_size_or_file){
+        if (is_number(file_number_or_test_number) && is_number(input_size)){
+            compare = false;
+            sorting_algorithms<T>::generate_input_files(
+                std::stoll(input_size),
+                std::stoll(file_number_or_test_number)
+            );
+            std::cout << "generated " << file_number_or_test_number
+                      << " files, each containing "
+                      << input_size << " random inputs\n";
+        } else {
+            throw std::runtime_error{help_message};
+        }
+    } else {
+        throw std::runtime_error{help_message};
+    }
 }
