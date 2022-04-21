2be26b31771b32642fedf1e29628b0430b5d520a
Oğuz Toraman <oguz.toraman@protonmail.com>
2022-04-21 20:58:58 +0300

    add license

--- main.cpp
+++ main.cpp
@@ -1,3 +1,25 @@
+/*
+ * main.cpp
+ *
+ * this file is a part of sorting algorithms project.
+ *
+ * Copyright (C) 2022 Oğuz Toraman <oguz.toraman@protonmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <main.hpp>
 
 using namespace project;
@@ -14,7 +36,7 @@ int main(int argc, char *argv[])
         default: throw std::runtime_error{help_message};
         }
         if (compare){
-            std::cout << algorithms.compare();
+            std::cout << algorithms.compare(algorithms.bubble | algorithms.heap);
         }
     } catch (const std::exception& e) {
         std::cerr << e.what() << "\n";
